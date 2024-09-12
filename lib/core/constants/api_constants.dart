f4262cb935cad60df54f85232ef1b5dca91eb8af
Ahmed Hamdi <ahmedhamdi725@gmail.com>
2024-09-12 22:16:22 +0300

    get tasks method implemented

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,3 +3,4 @@ const kBaseUrl = 'https://todo.iraqsapp.com';
 const kLoginEndpoint = '/auth/login';
 const kSignUpEndpoint = '/auth/register';
 const kRefreshTokenEndpoint = '/auth/refresh-token';
+const kTodosListEndpoint = '/todos';
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,43 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:todo_app/core/constants/api_constants.dart';
+import 'package:todo_app/core/constants/constants.dart';
 import 'package:todo_app/core/failures/failures.dart';
+import 'package:todo_app/core/services/api_services.dart';
 import 'package:todo_app/features/home/data/models/task_model.dart';
 import 'package:todo_app/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiServices _apiServices;
+  const HomeRepoImpl(this._apiServices);
+
   @override
-  Future<Either<Failure, List<TaskModel>>> getTasks(int page) {
-    // TODO: implement getTasks
-    throw UnimplementedError();
+  Future<Either<Failure, List<TaskModel>>> getTasks(int page) async {
+    try {
+      Response res = await _apiServices.get(
+        endPoint: kTodosListEndpoint,
+        queryParameters: {
+          'page': page,
+        },
+      );
+
+      if (res.statusCode == 200) {
+        List<TaskModel> tasks = [];
+
+        for (var element in res.data) {
+          tasks.add(TaskModel.fromJson(element));
+        }
+
+        return right(tasks);
+      } else {
+        return left(Failure(kUnknownErrorMessage));
+      }
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDio(e));
+      } else {
+        return left(Failure(kUnknownErrorMessage));
+      }
+    }
   }
 }
